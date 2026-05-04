1275f8c23e8905f32e363cb9d4da0465767f2423
Baddam Srujan Goud <srujan@Baddams-MacBook-Air.local>
2026-05-04 14:28:25 +0530

    switch case

---
+++ switchcase.js
@@ -0,0 +1,20 @@
+let num1=45
+let num2=55
+let operator="+";
+
+switch(operator){
+    case "+":
+        console.log(num1+num2);
+        break;
+    case "-":
+        console.log(num1-num2);
+        break;
+    case "*":
+        console.log(num1*num2);
+        break;
+    case "/":
+        console.log(num1/num2);
+        break;
+    default:
+        console.log("Invalid operator");
+}
